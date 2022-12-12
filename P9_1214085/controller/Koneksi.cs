1c51f915671693e3bf33a47e2699104fc1432a0e
fitrah <fitrahaliakbar@gmail.com>
2022-12-12 16:26:54 +0700

    Add project files.

---
+++ P9_1214085/controller/Koneksi.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_1214085
+{
+    internal class Koneksi
+    {
+        string connectionString = "Server=localhost;Database=ulbi;Uid=root;Pwd=;";
+        MySqlConnection kon;
+
+        public void OpenConnection()
+        {
+            kon = new MySqlConnection(connectionString);
+            kon.Open();
+        }
+
+        public void CloseConnection()
+        { 
+            kon.Close();
+        }
+
+        public void ExecuteQuery(string query)
+        {
+            MySqlCommand command = new MySqlCommand(query, kon);
+            command.ExecuteNonQuery();
+        }
+
+        public object ShowData(string query)
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, connectionString);
+            DataSet data = new DataSet();
+
+            adapter.Fill(data);
+            object datatable = data.Tables[0];
+            return datatable;
+        }
+    }
+}
---
+++ P9_1214085/view/Form1.cs
@@ -0,0 +1,149 @@
+﻿using P9_1214085.controller;
+using P9_1214085.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_1214085
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi =  new Koneksi(); 
+        M_mahasiswa m_mhs = new M_mahasiswa();
+        String id;
+
+        public void Tampil()
+        {
+            //Query DB
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+
+            //Mengubah Nama Kolom Tabel
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[1].HeaderText = "Nama";
+            DataMahasiswa.Columns[2].HeaderText = "Angkatan";
+            DataMahasiswa.Columns[3].HeaderText = "Alamat";
+            DataMahasiswa.Columns[4].HeaderText = "Email";
+            DataMahasiswa.Columns[5].HeaderText = "No HP";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Memanggil Method tampil
+            Tampil();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            if (npm.Text == "" || nama.Text == "" || angkatan.SelectedIndex == -1 || alamat.Text == "" || email.Text == "" || nohp.Text == "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                m_mhs.Npm = npm.Text;
+                m_mhs.Nama = nama.Text;
+                m_mhs.Angkatan = angkatan.Text;
+                m_mhs.Alamat = alamat.Text;
+                m_mhs.Email = email.Text;
+                m_mhs.Nohp = nohp.Text;
+
+                mhs.Insert(m_mhs);
+
+                npm.Text = "";
+                nama.Text = "";
+                angkatan.SelectedIndex = -1;
+                alamat.Text = "";
+                email.Text = "";
+                nohp.Text = "";
+
+                Tampil();
+            }
+        }
+
+        private void DataMahasiswa_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = DataMahasiswa.Rows[e.RowIndex].Cells[0].Value.ToString();
+            npm.Text = DataMahasiswa.Rows[e.RowIndex].Cells[0].Value.ToString();
+            nama.Text = DataMahasiswa.Rows[e.RowIndex].Cells[1].Value.ToString();
+            angkatan.Text = DataMahasiswa.Rows[e.RowIndex].Cells[2].Value.ToString();
+            alamat.Text = DataMahasiswa.Rows[e.RowIndex].Cells[3].Value.ToString();
+            email.Text = DataMahasiswa.Rows[e.RowIndex].Cells[4].Value.ToString();
+            nohp.Text = DataMahasiswa.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+            if (npm.Text == "" || nama.Text == "" || angkatan.SelectedIndex == -1 || alamat.Text == "" || email.Text == "" || nohp.Text == "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                m_mhs.Npm = npm.Text;
+                m_mhs.Nama = nama.Text;
+                m_mhs.Angkatan = angkatan.Text;
+                m_mhs.Alamat = alamat.Text;
+                m_mhs.Email = email.Text;
+                m_mhs.Nohp = nohp.Text;
+
+                mhs.Update(m_mhs, id);
+
+                npm.Text = "";
+                nama.Text = "";
+                angkatan.SelectedIndex = -1;
+                alamat.Text = "";
+                email.Text = "";
+                nohp.Text = "";
+
+                Tampil();
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            Tampil();
+
+            npm.Text = "";
+            nama.Text = "";
+            angkatan.SelectedIndex = -1;
+            alamat.Text = "";
+            email.Text = "";
+            nohp.Text = "";
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            DialogResult pesan = MessageBox.Show("Apakah anda yakin akan menghapus data ini?", "Perhatian", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (pesan == DialogResult.Yes)
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                mhs.Delete(id);
+                Tampil();
+            }
+        }
+
+        private void tbCariData_TextChanged(object sender, EventArgs e)
+        {
+            //query db search data
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa WHERE npm LIKE '%' '" + tbCariData.Text + "' '%' OR nama LIKE '%' '" + tbCariData.Text + "' '%'");
+        }
+    }
+}
